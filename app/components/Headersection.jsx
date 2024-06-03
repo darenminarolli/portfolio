d29dc85c721de314bb0ffa37f6b345683355e728
darenminarolli <80909893+darenminarolli@users.noreply.github.com>
2024-06-03 22:01:43 +0200

    update header section

--- app/components/Headersection.jsx
+++ app/components/Headersection.jsx
@@ -8,7 +8,7 @@ const Headersection = () => {
       <section id="#" className="w-full m-auto flex flex-col md:flex-row md:justify-between md:items-center mt-24">
 
         <div className=" flex flex-col text-wrap ">
-          <h1 className="text-6xl md:text-4xl  font-bold not-italic pb-6">Daren is a <span className="text-[#C778DD]">front-end developer</span></h1>
+          <h1 className="text-6xl md:text-4xl  font-bold not-italic pb-6">Daren is a <span className="text-[#C778DD]">web developer</span></h1>
           <p className="text-[#ABB2BF] break-words text-4xl md:text-2xl md:text-[16px] not-italic font-normal pb-8">He crafts responsive websites where technologies meet creativity</p>
           <Button endpoint={'#contacts'} name='Contact me!!' />
         </div>
